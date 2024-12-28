64436f94bdbb75ee4c2ff74d1b49433263579971
matthewjung04 <matthew.jung95@gmail.com>
2024-12-28 11:09:50 -0500

    menu page complete

--- src/pages/MenuPage/MenuPage.jsx
+++ src/pages/MenuPage/MenuPage.jsx
@@ -16,6 +16,25 @@ function MenuPage() {
         <Link className='menu__nav__links'>COLD DRINKS</Link>
         <Link className='menu__nav__links'>PASTERIES</Link>
       </article>
+      <article>
+        {
+          menuItems.map((menu) => (
+            <div id={menu.type} className='menu__items'>
+              <h1 className='menu__items__title'>{menu.type}</h1>
+              <div className='menu__items__block'>
+                <h2 className='menu__items__block__name'>{menu.items[0].name}</h2>
+                <p className='menu__items__block__desc'>{menu.items[0].description}</p>
+                <p className='menu__items__block__price'>{menu.items[0].price}</p>
+              </div>
+              <div className='menu__items__block'>
+                <h2 className='menu__items__block__name'>{menu.items[1].name}</h2>
+                <p className='menu__items__block__desc'>{menu.items[1].description}</p>
+                <p className='menu__items__block__price'>{menu.items[1].price}</p>
+              </div>
+            </div>
+          ))
+        }
+      </article>
     </section>
   )
 }
