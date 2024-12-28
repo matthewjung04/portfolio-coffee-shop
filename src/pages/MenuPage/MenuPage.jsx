7adafcd0d0952797fc229ba5af3540af1817e4a9
matthewjung04 <matthew.jung95@gmail.com>
2024-12-27 23:10:27 -0500

    add hero section to menu page

--- src/pages/MenuPage/MenuPage.jsx
+++ src/pages/MenuPage/MenuPage.jsx
@@ -1,7 +1,23 @@
-import './MenuPage'
+import { Link } from 'react-router-dom'
+import { menuItems } from '../../utils/utils'
+import './MenuPage.scss'
 
 function MenuPage() {
-
+  return (
+    <section className='menu'>
+      <article className='menu__hero'>
+        <div className='menu__hero__title'>
+          <div className='menu__hero__title__frame'></div>
+          <h1 className='menu__hero__title__text'>MENU</h1>
+        </div>
+      </article>
+      {/* <article>
+        <Link></Link>
+        <Link></Link>
+        <Link></Link>
+      </article> */}
+    </section>
+  )
 }
 
 export default MenuPage;
---
+++ src/utils/utils.jsx
@@ -0,0 +1,47 @@
+export const menuItems = [
+  {
+    type: "Hot Drinks",
+    items: [
+      {
+        name: "In-house Brew",
+        description: "Our in-house medium roast is a classic balance between sweet vanilla, nutty hazelnut, and a smooth low acidity to bring everything together.",
+        price: "$2.99"
+      },
+      {
+        name: "Expresso",
+        description: "A balance between bold and smooth, powerful and sweet, our signature espresso blend is a complex flavor profile. It features strong notes of semi-sweet chocolate and caramel, with mild acidity and a bright floral finish.",
+        price: "$4.99"
+      }      
+    ]
+  },
+  {
+    type: "Cold Drinks",
+    items: [
+      {
+        name: "Cold Brew",
+        description: "Our bright and delicious coffee blend, brewed over ice to give you a cold coffee freshness with all the flavor of our amazing in-house coffee.",
+        price: "$3.99"
+      },
+      {
+        name: "Iced Chai Latte",
+        description: "  A creamy drink with a kick, this drink features spicy chai tea blended with your choice of milk and poured over ice.",
+        price: "$6.99"
+      }
+    ]
+  },
+  {
+    type: "Pasteries",
+    items: [
+      {
+        name: "Bacon Oatmeal Raisin Cookie",
+        description: "An infused bacon, sugar and egg mixture is folded into a perfect blend of oats, flour, raisins and cinnamon to create a soft and chewy textured cookie.",
+        price: "$3.50"
+      },
+      {
+        name: "Blueberry, Lemon and Thyme Muffin",
+        description: "Tart and sweet, this classic blueberry and lemon combination comes with an earthy twist.",
+        price: "$2.50"
+      }
+    ]
+  }
+]
