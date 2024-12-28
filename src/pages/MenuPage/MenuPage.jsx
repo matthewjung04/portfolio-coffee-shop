ecc2c6b5beb5bc99d9f8211ca6194153affa6fb7
matthewjung04 <matthew.jung95@gmail.com>
2024-12-28 02:25:06 -0500

    add menu page navigation bar

--- src/pages/MenuPage/MenuPage.jsx
+++ src/pages/MenuPage/MenuPage.jsx
@@ -11,11 +11,11 @@ function MenuPage() {
           <h1 className='menu__hero__title__text'>MENU</h1>
         </div>
       </article>
-      {/* <article>
-        <Link></Link>
-        <Link></Link>
-        <Link></Link>
-      </article> */}
+      <article className='menu__nav'>
+        <Link className='menu__nav__links'>HOT DRINKS</Link>
+        <Link className='menu__nav__links'>COLD DRINKS</Link>
+        <Link className='menu__nav__links'>PASTERIES</Link>
+      </article>
     </section>
   )
 }
