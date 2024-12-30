a1234d764a4e9a54513f78ce5413189083f8287c
matthewjung04 <matthew.jung95@gmail.com>
2024-12-30 00:07:23 -0500

    add about us and discount sections to homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,7 +4,6 @@ import coffeeBean from '../../assets/images/coffeebeans.png'
 import coldBrew from '../../assets/images/coldbrew.png'
 import inHouseBrew from '../../assets/images/inhousebrew.png'
 import tiltedCan from '../../assets/images/tiltedcan.png'
-import tiltedCanBackground from '../../assets/images/coldbrewbackground.png'
 import coffeeBeanIcon from '../../assets/logos/coffee-beans.svg'
 import coffeeCupIcon from '../../assets/logos/coffee-cup.svg'
 import plantsIcon from '../../assets/logos/planting.svg'
@@ -62,11 +61,33 @@ function HomePage() {
 
       <article className='homepage__details'>
         <div className='homepage__details__about'>
-
+          <h1 className='homepage__details__about__title'>We're Different</h1>
+          <p className='homepage__details__about__desc'>
+            We grow and roast our own beans. Every bean, every cup,
+            every step of the way, we strive for excellence.
+          </p>
+          <div className='homepage__details__about__icons'>
+            <img src={plantsIcon} alt='plants' className='homepage__details__about__icons__image'/>
+            <img src={coffeeBeanIcon} alt='coffee beans' className='homepage__details__about__icons__image'/>
+            <img src={coffeeCupIcon} alt='coffee cup' className='homepage__details__about__icons__image'/>
+          </div>
+          <Link className='homepage__details__about__link'>
+            <button className='homepage__details__about__link--button'>
+              <div className='homepage__details__about__link--frame'>LEARN MORE</div>
+            </button>
+          </Link>
         </div>
-        <div className='homepage__details__discount'>
-
+        <div className='homepage__details__tilted-can'>
+          <img src={tiltedCan} alt='tilted can' className='homepage__details__tilted-can--image'/>
+          <div className='homepage__details__tilted-can--frame'></div>
         </div>
+      </article>
+
+      <article className='homepage__discount'>
+        <h1 className='homepage__discount__regular'>
+          Get 20% off your first order with code:
+          <span className='homepage__discount__bold'> FirstNitro</span>
+        </h1>
       </article>
     </section>
   )
